5ba4c601a15847a968de380269b046535b6cee00
carlosnsilva <cmanoel17@gmail.com>
2022-05-05 21:56:33 -0300

    first commit

---
+++ src/test/java/automation/ApplicationCucumber.java
@@ -0,0 +1,12 @@
+package automation;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "classpath:features", plugin = {"pretty","junit:src/test/resources/report/report-cucumber.xml"})
+public class ApplicationCucumber {
+
+
+}
